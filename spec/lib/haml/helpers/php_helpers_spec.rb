992e4c5f94798ff70a4f72b89a2118b110e33aef
Polarblau <polarblau@gmail.com>
2013-03-21 18:15:51 +0200

    Adding specs for convert_php_arguments.

--- spec/lib/haml/helpers/php_helpers_spec.rb
+++ spec/lib/haml/helpers/php_helpers_spec.rb
@@ -31,8 +31,38 @@ describe "Haml::Helpers" do
   end
 
   describe "User helpers" do
-    it "should be defined" do
+    it "should be defined"
+  end
 
+  # we'll be testing this private method as well,
+  # as it is essential to other parts and might even
+  # become public at some point
+  describe "#convert_php_arguments" do
+    it "should wrap string" do
+      view.send(:convert_php_arguments, "foo").should == "'foo'"
+    end
+
+    it "should not wrap PHP vars" do
+      view.send(:convert_php_arguments, "$foo").should == "$foo"
+    end
+
+    it "should not wrap Numbers" do
+      view.send(:convert_php_arguments, 123).should == 123
+    end
+
+    it "should convert hashes to assoc. arrays" do
+      view.send(:convert_php_arguments, :foo => "bar").
+        should == "array('foo' => 'bar')"
+    end
+
+    it "should convert nested hashes to nested assoc. arrays" do
+      view.send(:convert_php_arguments, :foo => { :bar => "bat" }).
+        should == "array('foo' => array('bar' => 'bat'))"
+    end
+
+    it "should convert PHP vars embedded in hashes" do
+      view.send(:convert_php_arguments, :foo => { :bar => "$bat" }).
+        should == "array('foo' => array('bar' => $bat))"
     end
   end
 
