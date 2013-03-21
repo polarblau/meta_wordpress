961f6465e537a7c6045fc03ade35981a8eb88af6
Polarblau <polarblau@gmail.com>
2013-03-21 16:27:52 +0200

    More layout tests to get started.

--- test/unit/layout_test.php
+++ test/unit/layout_test.php
@@ -1,6 +1,24 @@
 <?php
 require_once(dirname(__FILE__) . '/../../lib/templates/lib/meta_wordpress/layout.class.php');
 
+class TestOfSingletonLayout extends UnitTestCase {
+  function __construct() {
+    parent::__construct('Layout singleton tests');
+  }
+
+  function testStatePersists() {
+    $layout_1 = Layout::Instance();
+    $this->assertNotEqual($layout_1->layout, 'foo');
+
+    $layout_1->set_layout('foo');
+    $this->assertEqual($layout_1->layout, 'foo');
+
+    $layout_2 = Layout::Instance();
+    $this->assertEqual($layout_2->layout, 'foo');
+  }
+}
+
+
 class TestOfLayout extends UnitTestCase {
 
   function __construct() {
@@ -8,13 +26,19 @@ class TestOfLayout extends UnitTestCase {
   }
 
   function setUp() {
-    $this->layout = Layout::Instance();
+    $this->layout = new LayoutTestCase;
   }
 
   function testSetLayout() {
     $this->layout->set_layout('foo');
     $this->assertEqual($this->layout->layout, 'foo');
   }
+
+  function testSetLayoutAgain() {
+    $this->assertNotEqual($this->layout->layout, 'foo');
+  }
+
+  function testGetLayout() {}
 }
 
 ?>
