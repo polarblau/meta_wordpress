7d1d13208cef4d105f17b3bf6c8e0577542213a5
Polarblau <polarblau@gmail.com>
2013-03-22 01:35:09 +0200

    Adding specs for filters.

---
+++ spec/lib/haml/filters/docs_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe Haml::Filters::Docs do
+
+  it "should render single line docs" do
+    haml(":docs\n  Foobar").
+      should == "<?php\n/**\n * Foobar\n */\n?>\n"
+  end
+
+  it "should render multiple lines of docs" do
+    haml(":docs\n  Foobar\n  Barfoo").
+      should == "<?php\n/**\n * Foobar\n * Barfoo\n */\n?>\n"
+  end
+
+end
---
+++ spec/lib/haml/filters/php_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe Haml::Filters::PHP do
+
+  it "should render single line of PHP code" do
+    haml(":php\n  $foo = 'bar';").
+      should == "<?php\n  $foo = 'bar';\n?>\n"
+  end
+
+  it "should render multiple lines of PHP code" do
+    haml(":php\n  $foo = 'bar';\n  echo $foo;").
+      should == "<?php\n  $foo = 'bar';\n  echo $foo;\n?>\n"
+  end
+
+end
