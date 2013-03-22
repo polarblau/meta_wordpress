c7511a184af24f0f7e346cdcf419aa82a49c353e
Polarblau <polarblau@gmail.com>
2013-03-22 15:45:26 +0200

    No need to conditionally require dep.

--- lib/templates/view_helpers.rb
+++ lib/templates/view_helpers.rb
@@ -1,20 +1,4 @@
-<%- unless @skip_theme -%>
-require 'meta_wordpress/php_helpers'
-
-<%- end -%>
 module ViewHelpers
-  <%- if @skip_theme -%>
-  # If you’re feeling adventurous, you can include
-  # the PHP helpers module — check the docs for details:
-  #
-  # include MetaWordpress::PHPHelpers
-  #
-  # Don't forget to require the necessary file above
-  # using `require 'meta_wordpress/php_helpers'`
-  require 'meta_wordpress/php_helpers'
-  <%- else -%>
-  include MetaWordpress::PHPHelpers
-  <%- end -%>
 
   # Define your own helpers here!
 
