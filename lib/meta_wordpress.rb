0179478eec4669d6532e0daaf00e24508db777e7
Polarblau <polarblau@gmail.com>
2013-04-22 09:28:05 +0300

    Remove haml require all together.

--- lib/meta_wordpress.rb
+++ lib/meta_wordpress.rb
@@ -1,5 +1,4 @@
 require File.expand_path('guard/reload_meta_wordpress.rb', File.dirname(__FILE__))
-require 'haml'
 
 # we need to be able to reload the view helper
 load File.expand_path('haml/helpers.rb', File.dirname(__FILE__))
